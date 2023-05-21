5c94a279b06f594be5865c916d5ec09edb8fd4cc
driiisdev <olayinkayakub01@gmail.com>
2023-05-21 01:30:33 +0100

    web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,20 @@
-#!/usr/bin/env bash
-# Configure Nginx server to have a custom 404 page that contains the string Ceci n'est pas une page
+# Puppet manifest to install nginx
+package { 'nginx':
+  ensure => installed,
+}
 
-# Install nginx
-sudo apt-get update
-sudo apt-get install -y nginx
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
 
-# Creating Sample Page
-sudo echo "Hello World!" > /var/www/html/index.html
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
 
-# add the following configuration to the existing server block
-string_for_replacement="server_name _;\n\trewrite ^\/redirect_me https:\/\/www.google.com permanent;"
-sudo sed -i "s/server_name _;/$string_for_replacement/" /etc/nginx/sites-enabled/default
-
-# code for error page and redirect error 404
-sudo echo "Ceci n'est pas une page" > /var/www/html/404.html
-string_for_replacement="listen 80 default_server;\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}"
-sudo sed -i "s/listen 80 default_server;/$string_for_replacement/" /etc/nginx/sites-enabled/default
-
-# Restart Nginx
-sudo service nginx restart
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
