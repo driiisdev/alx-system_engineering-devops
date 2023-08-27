b12d5352f7fdcedc01e874f9c5c5c94304684d13
driiisdev <olayinkayakub01@gmail.com>
2023-08-27 06:59:03 +0100

    case sensitive

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,34 +6,41 @@ import requests
 
 BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
+
 def is_valid_subreddit(subreddit):
     '''Check if subreddit is valid'''
-    response = requests.get(BASE_URL.format(subreddit), headers={'User-agent': 'Unix:0-subs:v1'})
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers={'User-agent': 'Unix:0-subs:v1'})
     return response.status_code == 200
+
 
 def count_words(subreddit, word_list, hot_list=[], after=None):
     '''Get ALL hot posts'''
     headers = {'User-agent': 'Unix:0-subs:v1'}
     params = {'limit': 100}
-    
+
     if isinstance(after, str):
         if after != "STOP":
             params['after'] = after
         else:
             return print_results(word_list, hot_list)
 
-    response = requests.get(BASE_URL.format(subreddit), headers=headers, params=params)
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers=headers, params=params)
     if response.status_code != 200:
         return None
-    
+
     data = response.json().get('data', {})
+    data.lower()
     after = data.get('after', 'STOP')
     if not after:
         after = "STOP"
-    
-    hot_list = hot_list + [post.get('data', {}).get('title') for post in data.get('children', [])]
-    
+
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+
     return count_words(subreddit, word_list, hot_list, after)
+
 
 def print_results(word_list, hot_list):
     '''Prints request results'''
@@ -42,10 +49,11 @@ def print_results(word_list, hot_list):
         count[word] = 0
     for title in hot_list:
         for word in word_list:
-            count[word] = count[word] + len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
-    
+            count[word] = count[word] + len(re.findall(
+                r'(?:^| ){}(?:$| )'.format(word), title, re.I))
+
     count = {k: v for k, v in count.items() if v > 0}
     words = sorted(list(count.keys()))
-    
+
     for word in sorted(words, reverse=True, key=lambda k: count[k]):
         print("{}: {}".format(word, count[word]))
