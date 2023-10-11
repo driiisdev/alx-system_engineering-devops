f3f10883a6c7fd8ce225418bc4407e6f410022a9
driiisdev <olayinkayakub01@gmail.com>
2023-10-11 23:31:19 +0100

    checksi/o

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,32 @@
-#!/usr/bin/python3
-'''Get top 10 hot posts'''
-import pprint
 import requests
 
 BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
-
 def top_ten(subreddit):
-    '''Get top 10 hot posts'''
-    headers = {'User-agent': 'Unix:0-subs:v1'}
+    """Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit: The name of the subreddit to query.
+    """
+
     response = requests.get(BASE_URL.format(subreddit),
-                            headers=headers)
+                            allow_redirects=False)
 
     # Check for 404 status code
     if response.status_code == 404:
-        print("Subreddit does not exist")
+        print(None)
+        return
+
+    # Check for redirect
+    if response.headers.get('Location'):
+        print(None)
         return
 
     data = response.json().get('data', {}).get('children', {})
     if response.status_code != 200 or not data:
-        print("No hot posts")
+        print(None)
         return
 
-    # Print the top 10 hot posts
+    # Print the titles of the top 10 hot posts
     for post in data[0:10]:
         print(post.get('data', {}).get('title'))
-
-
-if __name__ == '__main__':
-    subreddit = input("Enter subreddit name: ")
-    top_ten(subreddit)
